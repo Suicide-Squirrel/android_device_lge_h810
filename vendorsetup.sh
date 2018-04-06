7b50a1db26914714e05a744d09c2e2c6920f9a50
steadfasterX <steadfastX@boun.cr>
2018-04-06 09:43:14 +0200

    fix: correcting vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,6 @@
-add_lunch_combo cm_h810-userdebug
-add_lunch_combo cm_h810-eng
+export ANDROID_SET_JAVA_HOME=true
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export USE_NINJA=true
+
+add_lunch_combo aoscp_h810_usu-userdebug
+add_lunch_combo aoscp_h810_usu-eng
