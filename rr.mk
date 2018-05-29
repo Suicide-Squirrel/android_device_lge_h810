0ac2f0b2878386b5bf261d206ffe009d3cb9197b
steadfasterX <steadfastX@boun.cr>
2018-05-29 15:00:53 +0200

    h810: RR bring up
    (cherry picked from commit 547c8fd3e53122c1accb1dcd5de9cc8237bad77d)
    
    Change-Id: I4729a85fe3d10f2e30f29f877beb0686166d8c62

--- rr.mk
+++ rr.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/h810_usu/full_h810.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h810_usu
+PRODUCT_NAME := rr_h810_usu
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
@@ -14,5 +14,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="lge/p1__tmo_us/p1:7.0/NRD90U/1717709471d0f:user/release-keys" \
     PRIVATE_BUILD_DESC="p1_global_com-user 8.0 NRD90U 171931902b165 release-keys"
 
-BUILD_FINGERPRINT="lge/p1_global_com/p1:8.0/NRD90U/171931902b165:user/release-keys"
-
+BUILD_FINGERPRINT := "lge/p1__tmo_us/p1:8.1.0/OPM4.171019.016.A1/4720811:user/release-keys"
