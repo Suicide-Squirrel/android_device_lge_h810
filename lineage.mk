2730802bb180491eb056674000dc7ac84903d53c
steadfasterX <steadfastX@boun.cr>
2018-07-06 16:43:46 +0200

    fix signature
    https://github.com/Suicide-Squirrel/issues_oreo/issues/15
    
    Change-Id: I3cb22d124a8531c92641cf3e0991c51ab39f34a1

--- lineage.mk
+++ lineage.mk
@@ -11,8 +11,7 @@ PRODUCT_NAME := lineage_h810_usu
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g4" \
     PRODUCT_NAME="p1" \
-    BUILD_FINGERPRINT="lge/p1__tmo_us/p1:7.0/NRD90U/1717709471d0f:user/release-keys" \
-    PRIVATE_BUILD_DESC="p1_global_com-user 8.0 NRD90U 171931902b165 release-keys"
+    PRIVATE_BUILD_DESC="p1_att_us-user 6.0 MRA58K 1617209012672 release-keys"
 
-BUILD_FINGERPRINT := "lge/p1_global_com/p1:8.0/NRD90U/171931902b165:user/release-keys"
+BUILD_FINGERPRINT := "lge/p1_att_us/p1:6.0/MRA58K/1617209012672:user/release-keys"
 
