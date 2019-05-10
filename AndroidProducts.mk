9cd2d534bacb330e4a1f2ccd26735d8bed4e39e5
steadfasterX <steadfasterX@gmail.com>
2019-05-10 17:41:12 +0200

    h810: setup for lineage-16.0
    Change-Id: Ia88b268e251fb6f5ea1a51b7ddc2ec7263680fad

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The CyanogenMod Project
+# Copyright 2015 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,16 +14,5 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from device
-$(call inherit-product, device/lge/h810_usu/device.mk)
-
-# Set those variables here to overwrite the inherited values.
-PRODUCT_DEVICE := h810_usu
-PRODUCT_NAME := full_h810
-PRODUCT_BRAND := lge
-PRODUCT_MODEL := LG-H810
-PRODUCT_MANUFACTURER := LGE
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/lineage_h810.mk
